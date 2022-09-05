f4ba61a0c6ac9ed30a20fa6963817bfd3c8797b8
krzysztofrz <34165654+krzysztofrz@users.noreply.github.com>
2022-09-05 23:31:44 +0200

    creating html content

--- src/js/htmlContent/mainSection.js
+++ src/js/htmlContent/mainSection.js
@@ -2,9 +2,17 @@ const mainSection = document.querySelector('#mainSection');
 mainSection.insertAdjacentHTML('beforeend', `                                      
 
     <!-- your HTML goes here -->                                                         
-    <p>my main section </p>    
-    <h1>test
-        <p>green</p>
-    </h1>                                        
+ <section class="section">
+    <main class="main">
+        <div class="sectionImg">
+            <img src="../../../images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_ar_1_1,c_fill,g_auto__c_scale,w_200.jpg" alt="sunset on the beach">
+        </div>
+        <div class="sectionContent">
+            <h1 class="contentTitle">Lorem Ipsunm</h1>
+            <p class="contentText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, atque eos esse fugit obcaecati pariatur porro quibusdam repudiandae temporibus voluptatibus.</p>
+            <button class="btn contentButton">Button</button>
+        </div>
+    </main>  
+</section>                                     
                                                                                 
 `);
